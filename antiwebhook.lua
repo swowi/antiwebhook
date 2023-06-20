9b373141d7e7d03ceb6d26dbaec81c87508ea796
swow <115572200+SwegCheems@users.noreply.github.com>
2023-06-20 23:01:16 +0300

    Add files via upload

---
+++ antiwebhook.lua
@@ -0,0 +1,15 @@
+local webhooks = { "discord.com/api/webhooks", "discordapp.com/api/webhooks" }
+local request = http_request or request or HttpPost or syn and syn.request or http and http.request
+
+rconsolename("swow's anti-webhook")
+rconsolewarn("anti-webhook loaded")
+rconsolewarn("blocks & deletes any webhook logging attempts")
+webhookdetect = hookfunction(request, function(self)
+    if self.Method and self.Method == "POST" and self.Url and not table.find(webhooks, self.Url) then
+        rconsolewarn("1 webhook request blocked & webhook deleted")
+        request{ Url = self.Url, Method = "DELETE" }
+        self.Url = ""
+    end
+    return webhookdetect(self)
+end)
+
